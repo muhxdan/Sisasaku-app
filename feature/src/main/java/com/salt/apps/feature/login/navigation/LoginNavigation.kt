d490183c09bf010b8663b5c7813ae2bde3c39fa8
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-12-09 11:43:52 +0700

    Initial feature login

---
+++ feature/src/main/java/com/salt/apps/feature/login/navigation/LoginNavigation.kt
@@ -0,0 +1,17 @@
+package com.salt.apps.feature.login.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.salt.apps.feature.login.LoginScreen
+
+const val loginNavigationRoute = "login_route"
+
+fun NavGraphBuilder.loginGraph(
+    onLoginClick: () -> Unit,
+) {
+    composable(
+        route = loginNavigationRoute
+    ) {
+        LoginScreen()
+    }
+}
