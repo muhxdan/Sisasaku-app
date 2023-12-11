48ef7850d94da1e5bf2a95ba336546103baa02c5
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-12-11 20:59:15 +0700

    Add supabase module

---
+++ core/src/main/java/com/salt/apps/core/di/SupabaseModule.kt
@@ -0,0 +1,33 @@
+package com.salt.apps.core.di
+
+import com.salt.apps.core.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.compose.auth.ComposeAuth
+import io.github.jan.supabase.compose.auth.googleNativeLogin
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.GoTrue
+import io.github.jan.supabase.postgrest.Postgrest
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SupabaseModule {
+    @Provides
+    @Singleton
+    fun provideSupabaseClient(): SupabaseClient {
+        return createSupabaseClient(
+            supabaseUrl = BuildConfig.SUPABASE_URL,
+            supabaseKey = BuildConfig.SUPABASE_KEY
+        ) {
+            install(GoTrue)
+            install(ComposeAuth) {
+                googleNativeLogin(serverClientId = BuildConfig.GOOGLE_CLIENT_ID)
+            }
+            install(Postgrest)
+        }
+    }
+}
