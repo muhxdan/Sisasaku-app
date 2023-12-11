fc8f098b0fe6e8ff5bacb9396fc293017d36bc71
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-12-11 20:49:56 +0700

    Add splash screen v1.0

--- app/src/main/java/com/salt/apps/sisasaku/ui/MainActivity.kt
+++ app/src/main/java/com/salt/apps/sisasaku/ui/MainActivity.kt
@@ -3,28 +3,29 @@ package com.salt.apps.sisasaku.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.salt.apps.sisasaku.ui.navigation.SsNavHost
 import com.salt.apps.sisasaku.ui.theme.SisasakuTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private lateinit var appState: SsAppState
-    private var keepSplashOpened = true
+    private var splashScreenClosed = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen().setKeepOnScreenCondition {
-            keepSplashOpened
+            !splashScreenClosed
         }
         super.onCreate(savedInstanceState)
         setContent {
-            appState = rememberSsAppState()
+            LaunchedEffect(key1 = Unit) {
+                delay(2000)
+                splashScreenClosed = true
+            }
             SisasakuTheme {
-                SsNavHost(
-                    appState = appState,
-                    onDataLoaded = {
-                        keepSplashOpened = false
-                    }
-                )
+                SsNavHost()
             }
         }
     }
