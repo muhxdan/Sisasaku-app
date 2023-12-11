32ffd625d3a2840a1f132c4dc9f0e95a42cc2168
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-12-11 20:42:17 +0700

    Add state class

---
+++ core/src/main/java/com/salt/apps/core/data/State.kt
@@ -0,0 +1,7 @@
+package com.salt.apps.core.data
+
+sealed class State<out T> {
+    data class Loading<out T>(val data: T? = null) : State<T>()
+    data class Success<out T>(val data: T) : State<T>()
+    data class Error<out T>(val message: String, val data: T? = null) : State<T>()
+}
