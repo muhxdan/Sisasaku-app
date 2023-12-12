c8303f72f1cd09239c7d69bbd2bd616e4e7ae94b
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-12-12 21:01:06 +0700

    Add session naviation app

--- app/src/main/java/com/salt/apps/sisasaku/ui/main/MainActivity.kt
+++ app/src/main/java/com/salt/apps/sisasaku/ui/main/MainActivity.kt
@@ -1,9 +1,12 @@
-package com.salt.apps.sisasaku.ui
+package com.salt.apps.sisasaku.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.salt.apps.sisasaku.ui.navigation.SsNavHost
 import com.salt.apps.sisasaku.ui.theme.SisasakuTheme
@@ -12,20 +15,26 @@ import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private var splashScreenClosed = false
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen().setKeepOnScreenCondition {
-            !splashScreenClosed
+            !mainViewModel.splashScreenClosed.value
         }
         super.onCreate(savedInstanceState)
         setContent {
+            val accessToken by mainViewModel.accessToken.collectAsState()
+
             LaunchedEffect(key1 = Unit) {
-                delay(2000)
-                splashScreenClosed = true
+                delay(1000)
+                mainViewModel.splashScreenClosed.value = true
             }
             SisasakuTheme {
-                SsNavHost()
+                if (accessToken != null) {
+                    SsApp()
+                } else {
+                    SsNavHost()
+                }
             }
         }
     }
