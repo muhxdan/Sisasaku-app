36b27180fc6b2cb8621947d03b979781cf6cbaa1
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2023-12-13 20:59:52 +0700

    Add constants database

---
+++ core/src/main/java/com/salt/apps/core/utilities/Constants.kt
@@ -0,0 +1,7 @@
+package com.salt.apps.core.utilities
+
+object Constants {
+    const val DB_NAME = "Sisasaku.db"
+    const val DB_VERSION = 1
+    const val ENCRYPTED_DB_PASSPHRASE = "sisasaku"
+}
