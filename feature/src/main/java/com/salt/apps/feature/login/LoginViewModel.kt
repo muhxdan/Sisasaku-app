6ea6ca4d7874f2efae9aebb3e0d037dddaec67d8
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-01-09 13:42:05 +0700

    Update auth login viewmodel

--- feature/src/main/java/com/salt/apps/feature/login/LoginViewModel.kt
+++ feature/src/main/java/com/salt/apps/feature/login/LoginViewModel.kt
@@ -8,27 +8,29 @@ import com.salt.apps.core.domain.usecase.AuthUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.compose.auth.composable.NativeSignInResult
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val authUseCase: AuthUseCase,
-    private val supabaseClient: SupabaseClient
+    private val supabaseClient: SupabaseClient,
 ) : ViewModel() {
-    private val _loginState: MutableStateFlow<State<UserResponse>?> = MutableStateFlow(null)
-    val loginState: StateFlow<State<UserResponse>?> get() = _loginState
+    private val _loginState: MutableStateFlow<State<UserResponse>> = MutableStateFlow(State.Empty)
+    val loginState: StateFlow<State<UserResponse>> get() = _loginState.asStateFlow()
 
     fun signInWithGoogle(result: NativeSignInResult) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             authUseCase.signinWithGoogle(result, supabaseClient)
-                .collectLatest { state -> _loginState.value = state }
+                .collect { state ->
+                    _loginState.value = state
+                }
         }
     }
 
     fun getSupabaseClient(): SupabaseClient = supabaseClient
 }
-
