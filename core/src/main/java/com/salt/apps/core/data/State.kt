a8b7522c10dc49f92fd9e77a11b84b5c308b7f37
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-01-09 11:05:21 +0700

    Add state empty

--- core/src/main/java/com/salt/apps/core/data/State.kt
+++ core/src/main/java/com/salt/apps/core/data/State.kt
@@ -4,4 +4,5 @@ sealed class State<out T> {
     data class Loading<out T>(val data: T? = null) : State<T>()
     data class Success<out T>(val data: T) : State<T>()
     data class Error<out T>(val message: String, val data: T? = null) : State<T>()
+    data object Empty : State<Nothing>()
 }
