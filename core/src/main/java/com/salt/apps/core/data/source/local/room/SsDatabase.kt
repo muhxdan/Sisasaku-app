513fed0fbd3db520484ebee2fb56e70e3e006cdb
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-01-11 21:42:50 +0700

    Update database local

--- core/src/main/java/com/salt/apps/core/data/source/local/room/SsDatabase.kt
+++ core/src/main/java/com/salt/apps/core/data/source/local/room/SsDatabase.kt
@@ -2,15 +2,16 @@ package com.salt.apps.core.data.source.local.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.salt.apps.core.data.source.local.entity.Entity
+import com.salt.apps.core.data.source.local.entity.UserEntity
+import com.salt.apps.core.data.source.local.room.dao.UserDao
 import com.salt.apps.core.utilities.Constants.DB_VERSION
 
 @Database(
-    entities = [Entity::class],
+    entities = [UserEntity::class],
     version = DB_VERSION,
     exportSchema = false
 )
 
 abstract class SsDatabase : RoomDatabase() {
-    abstract fun ssDao(): SsDao
+    abstract fun userDao(): UserDao
 }
