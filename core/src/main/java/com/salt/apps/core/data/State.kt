a2c5caf653e33a75e913d7f0ff86c29291194948
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-01-11 21:43:35 +0700

    Add initial state

--- core/src/main/java/com/salt/apps/core/data/State.kt
+++ core/src/main/java/com/salt/apps/core/data/State.kt
@@ -2,7 +2,7 @@ package com.salt.apps.core.data
 
 sealed class State<out T> {
     data class Loading<out T>(val data: T? = null) : State<T>()
-    data class Success<out T>(val data: T) : State<T>()
+    data class Success<out T>(val data: T? = null) : State<T>()
     data class Error<out T>(val message: String, val data: T? = null) : State<T>()
-    data object Empty : State<Nothing>()
+    data object Initial : State<Nothing>()
 }
