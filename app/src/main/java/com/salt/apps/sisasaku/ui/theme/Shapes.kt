c2e1e8ba941bc821455a8a0ab2faf9448695fa88
Muhammad Danu Firdaus <danufirdaus51@gmail.com>
2024-01-14 16:40:23 +0700

    Update shapes

--- app/src/main/java/com/salt/apps/sisasaku/ui/theme/Shapes.kt
+++ app/src/main/java/com/salt/apps/sisasaku/ui/theme/Shapes.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.unit.dp
 val shapes = Shapes(
     extraSmall = RoundedCornerShape(4.dp),
     small = RoundedCornerShape(8.dp),
-    medium = RoundedCornerShape(16.dp),
+    medium = RoundedCornerShape(14.dp),
     large = RoundedCornerShape(24.dp),
     extraLarge = RoundedCornerShape(32.dp)
 )
